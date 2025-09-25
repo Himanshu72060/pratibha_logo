4bb1089302a7e74b92b675700191ecdde9655441
Himanshu72060 <himanshusharma00898@gmail.com>
2025-09-25 15:02:04 +0530

    hj

--- routes/counterRoutes.js
+++ routes/counterRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const path = require("path");
 
 const {
     createCounter,
@@ -10,19 +9,8 @@ const {
     deleteCounter,
 } = require("../controllers/counterController");
 
-// Multer setup
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        cb(
-            null,
-            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-        );
-    },
-});
-
+// Multer setup for file upload
+const storage = multer.diskStorage({});
 const upload = multer({ storage });
 
 // Routes
