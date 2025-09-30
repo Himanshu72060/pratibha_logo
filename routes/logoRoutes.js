25ad33a8f545dfcf3ace5074a0692e1164ba4ee1
Himanshu72060 <himanshusharma00898@gmail.com>
2025-09-30 12:18:09 +0530

    ok

--- routes/logoRoutes.js
+++ routes/logoRoutes.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 const upload = require("../middleware/upload");
+
+
 const {
     createLogo,
     getLogos,
     updateLogo,
     deleteLogo,
 } = require("../controllers/logoController");
+
+
+// ✅ Multer config (disk storage, temporary file path)
+const storage = multer.diskStorage({});
+const upload = multer({ storage });
 
 router.post("/", upload.single("image"), createLogo);
 router.get("/", getLogos);
